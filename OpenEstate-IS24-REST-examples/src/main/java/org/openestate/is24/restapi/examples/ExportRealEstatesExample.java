3ea24a2003b5530ce016a6f93099e10d83b81b3d
Andreas Rudolph <andy@openindex.de>
2014-07-27 13:54:44 +0200

    added subproject with examples

---
+++ OpenEstate-IS24-REST-examples/src/main/java/org/openestate/is24/restapi/examples/ExportRealEstatesExample.java
@@ -0,0 +1,91 @@
+/*
+ * Copyright 2014 OpenEstate.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.openestate.is24.restapi.examples;
+
+import java.io.IOException;
+import javax.xml.bind.JAXBException;
+import oauth.signpost.exception.OAuthException;
+import org.openestate.is24.restapi.AbstractClient;
+import org.openestate.is24.restapi.DefaultClient;
+import org.openestate.is24.restapi.ImportExport;
+import org.openestate.is24.restapi.utils.RequestFailedException;
+import org.openestate.is24.restapi.xml.XmlUtils;
+import org.openestate.is24.restapi.xml.offerlistelement.OfferRealEstateForList;
+import org.openestate.is24.restapi.xml.realestates.RealEstates;
+
+public class ExportRealEstatesExample
+{
+  final static String WEBSERVICE_URL = AbstractClient.LIVE_API;
+  final static String CONSUMER_KEY = "my consumer key";
+  final static String CONSUMER_SECRET = "my consumer secret";
+  final static String ACCESS_KEY = "user's access key";
+  final static String ACCESS_SECRET = "user's access secret";
+
+  public static void main( String[] args )
+  {
+    AbstractClient client = new DefaultClient(
+      WEBSERVICE_URL, CONSUMER_KEY, CONSUMER_SECRET );
+
+    // authorize at the webservice with the access token
+    try
+    {
+      client.authorize( ACCESS_KEY, ACCESS_SECRET );
+    }
+    catch (OAuthException ex)
+    {
+      throw new RuntimeException( "Authorization failed!", ex );
+    }
+
+    // get list of real estates for a certain agent
+    int page = 0;
+    RealEstates result = null;
+    do
+    {
+      page++;
+      try
+      {
+        result = ImportExport.RealEstateService.getAll( client, null, null, 10, 0, true );
+        for (OfferRealEstateForList object : result.getRealEstateList().getRealEstateElement())
+        {
+          String xml = XmlUtils.marshal( object, "UTF-8", true );
+          System.out.println( "------------------" );
+          System.out.println( "object id " + object.getId() + "" );
+          System.out.println( "object title '" + object.getTitle() +  "'" );
+          System.out.println( xml );
+        }
+      }
+      catch (RequestFailedException ex)
+      {
+        throw new RuntimeException( "Request failed with "
+          + "'" + ex.statusMessage+"' (" + ex.statusCode + ")!", ex );
+      }
+      catch (OAuthException ex)
+      {
+        throw new RuntimeException( "Authorization failed!", ex );
+      }
+      catch (JAXBException ex)
+      {
+        throw new RuntimeException( "XML processing failed!", ex );
+      }
+      catch (IOException ex)
+      {
+        throw new RuntimeException( "Can't get object list!", ex );
+      }
+    }
+    while (result.getPaging().getNumberOfPages()>page);
+  }
+}
---
+++ OpenEstate-IS24-REST-examples/src/main/java/org/openestate/is24/restapi/examples/HttpComponents42ClientExample.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2014 OpenEstate.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.openestate.is24.restapi.examples;
+
+import org.openestate.is24.restapi.AbstractClient;
+import org.openestate.is24.restapi.hc42.HttpComponents42Client;
+
+public class HttpComponents42ClientExample
+{
+  final static String WEBSERVICE_URL = AbstractClient.LIVE_API;
+  final static String CONSUMER_KEY = "my consumer key";
+  final static String CONSUMER_SECRET = "my consumer secret";
+
+  public static void main( String[] args )
+  {
+    AbstractClient client = new HttpComponents42Client(
+      WEBSERVICE_URL, CONSUMER_KEY, CONSUMER_SECRET );
+
+    // do something useful with the client
+  }
+}
