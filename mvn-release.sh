27df9ae9f55f6a47a8e603cbbd157181ad49b6e1
Andreas Rudolph <andy@openindex.de>
2014-10-28 19:28:20 +0100

    update "docs" submodule when a new release is packaged

--- mvn-release.sh
+++ mvn-release.sh
@@ -2,10 +2,14 @@
 
 MVN=mvn
 PROJECT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# force an update of submodules
+$PROJECT_DIR/git-submodule-update.sh
+
+# install library and build release package
 export LANG=en
 set -e
 
 cd $PROJECT_DIR
-./git-submodule-update.sh
 $MVN clean install javadoc:aggregate
 $MVN --non-recursive assembly:single
