c0d836403915f6f8554e7b4bd68fdd917d925aab
Andreas Rudolph <andy@openindex.de>
2015-01-12 02:47:41 +0100

    updated license headers

--- OpenEstate-IS24-REST-webapp/src/test/java/org/openestate/is24/restapi/webapp/package-info.java
+++ OpenEstate-IS24-REST-webapp/src/test/java/org/openestate/is24/restapi/webapp/package-info.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014 OpenEstate.org.
+ * Copyright 2014-2015 OpenEstate.org.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,5 +13,4 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
-package org.openestate.is24.restapi;
+package org.openestate.is24.restapi.webapp;
