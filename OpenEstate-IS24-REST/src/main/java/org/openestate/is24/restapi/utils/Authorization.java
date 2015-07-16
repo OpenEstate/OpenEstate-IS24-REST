5ea7ade883db91bc1c63acaef791ee10cf56fd8c
Andreas Rudolph <andy@openindex.de>
2015-07-16 05:03:28 +0200

    Authorization & Verification class should implement Serializable

--- OpenEstate-IS24-REST/src/main/java/org/openestate/is24/restapi/utils/Authorization.java
+++ OpenEstate-IS24-REST/src/main/java/org/openestate/is24/restapi/utils/Authorization.java
@@ -1,4 +1,4 @@
-/* 
+/*
  * Copyright 2014-2015 OpenEstate.org.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -15,14 +15,17 @@
  */
 package org.openestate.is24.restapi.utils;
 
+import java.io.Serializable;
+
 /**
  * Token for authentication at the Webservice.
  *
  * @since 0.1
  * @author Andreas Rudolph <andy@openindex.de>
  */
-public class Authorization
+public class Authorization implements Serializable
 {
+  private final static long serialVersionUID = 1L;
   //private final static Logger LOGGER = LoggerFactory.getLogger( Authorization.class );
   public final String accessToken;
   public final String accessTokenSecret;
